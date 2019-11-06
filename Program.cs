910379e21daaf8521762260163ce40289d5f8432
Afzaal Ahmad Zeeshan <afzaal.ahmad.zeeshan@hotmail.com>
2019-11-06 05:12:08 +0500

    Added the Sentry SDK.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sentry;
+
+namespace hello_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SentrySdk.Init("https://<hidden>@sentry.io/<hidden>"))
+            {
+                int a = 1;
+                int b = 0;
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
